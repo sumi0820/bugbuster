67267b5ff56e9a1ac3ef2942eaf01d62f6148ddc
Sumiya Ushiro <u.sumi0820@gmail.com>
2020-10-23 15:41:14 +0200

    first depoloy

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI ||'mongodb://localhost/'
 
 mongoose
-  .connect('mongodb://localhost/bugbuster', {useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
